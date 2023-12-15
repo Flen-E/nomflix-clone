41ef71a7785b6a106b75c58016e4a094ee608480
Flen-E <cjj642@naver.com>
2023-12-16 00:52:21 +0900

    update header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,34 @@
+import styled from "styled-components";
+
+const Nav = styled.nav``;
+
+const Col = styled.div``;
+
+const Logo = styled.div``;
+
+const Items = styled.div``;
+
+const Item = styled.div``;
+
+
+
 function Header() {
-    return null;
+    return (
+        <Nav>
+            <Col>
+                <Logo/>
+                <Items>
+                    <Item>Home</Item>
+                    <Item>Tv Shows</Item>
+                </Items>
+            </Col>
+            <Col>
+                <button>
+
+                </button>
+            </Col>
+        </Nav>
+    )
+
 }
 export default Header;
